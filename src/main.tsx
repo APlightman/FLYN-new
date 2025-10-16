ce046a8c2f2e9f896477770d88487cbbc03ffd4b
APlightman <thebobyzi@gmail.com>
2025-10-17 00:38:53 +0300

    fix: resolve build issues and improve build process

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './critical.css'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
